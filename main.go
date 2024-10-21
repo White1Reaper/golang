7188d4a8a49136bca4470e3bd49086b8a3398590
White1Reaper <113230473+White1Reaper@users.noreply.github.com>
2024-10-21 19:01:56 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"https://github.com/White1Reaper/golang/blob/main/packages/mutex"
+	"https://github.com/White1Reaper/golang/blob/main/packages/channel"
+	"https://github.com/White1Reaper/golang/blob/main/packages/atomic"
+)
+
+func main() {
+	mutex.Mutex()
+	channel.Channel()
+	atomic.Atomic()
+}
